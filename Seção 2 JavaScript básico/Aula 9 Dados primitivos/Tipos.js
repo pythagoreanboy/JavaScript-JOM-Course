1e9220af57c53cc65e32e97ef39493a4990a2969
Bruno <brunodesousasilvai@gmail.com>
2021-11-05 00:19:07 -0300

    texto adicionado

--- Seção 2 JavaScript básico/Aula 9 Dados primitivos/Tipos.js
+++ Seção 2 JavaScript básico/Aula 9 Dados primitivos/Tipos.js
@@ -1,10 +1,10 @@
 // Dados primitivos em javascript : string, number, undefined, boolean, null, symbol. 
 const nome = 'Luiz'; // string
 const nome1 = "Luiz"; // string
-const nome2 = `Luiz`; // string
+const nome2 = `Luiz`; // (crase) template string
 const num1 = 10; // number
-const num2 = 10.52 // number
-let nomeAluno; // undefined -> não aponta pra local nenhum na memória.
+const num2 = 10.52; // number
+let nomeAluno; // undefined -> não aponta pra local nenhum na memória, valor indefinido.
 const sobrenomeAluno = null; //Null -> não aponta pra local nenhum na memória.
 const aprovado = false; // Boolean = true, false (lógico).
 console.log(typeof num1, num1); // typeof mostra o tipo do elemento.
